2eadeab3896250993168d2089969f5dbf5e85990
geonxee <1913kg@naver.com>
2023-02-21 16:59:36 +0900

    feat: Mainpage foundation

--- shfrontend/src/views/HomeView.vue
+++ shfrontend/src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="page">
+        <header></header>
+        <nav>
+            <div class="main">
+              <img src="../assets/main/main.png">
+              <button class="menu" onclick=""></button>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      modalOpen: true,
+    }
   }
 }
 </script>
+
+<style scoped>
+.page {
+    margin: 0 auto;
+    margin-top: 30px;
+    max-width: 375px;
+}
+nav {
+    padding: 0;
+    position: relative;
+}
+.menu {
+  position: relative;
+  float: left;
+  top: -40px;
+  left: 2px;
+  height: 40px;
+  width: 50px;
+  background-color: transparent;
+  border-color: transparent;
+}
+
+img {
+  position: relative;
+}
+
+</style>
