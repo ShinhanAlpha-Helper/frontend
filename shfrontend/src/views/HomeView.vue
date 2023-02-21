858da222e863c649f8272bf07ee687a9eac780c1
GeonheeKim <1913kg@naver.com>
2023-02-21 22:07:16 +0900

    feat: Complement MainPage

--- shfrontend/src/views/HomeView.vue
+++ shfrontend/src/views/HomeView.vue
@@ -3,8 +3,15 @@
         <header></header>
         <nav>
             <div class="main">
-              <img src="../assets/main/main.png">
-              <button class="menu" onclick=""></button>
+                <img src="../assets/main/main.png">
+                <button class="menu" onclick=""></button>
+            </div>
+            <!-- 모달창 -->
+            <div class="black-bg" v-if="modalOpen === true">
+                <img class="modalOpen" src="../assets/main/modal.png">
+                <button class="modalVoca" style="cursor: pointer;" onclick="location.href=window.location.href+'vocamain'">단어</button>
+                <button v-on:click="modalOpen = false" class="modal-exit-btn">닫기</button>
+                <button v-on:click="modalOpen = false" class="modal-exit-btn">닫기</button>
             </div>
         </nav>
     </div>
@@ -14,9 +21,9 @@
 export default {
   data() {
     return {
-      modalOpen: true,
+      modalOpen: true
     }
-  }
+  },
 }
 </script>
 
@@ -29,6 +36,7 @@ export default {
 nav {
     padding: 0;
     position: relative;
+    height: 631px;
 }
 .menu {
   position: relative;
@@ -40,9 +48,39 @@ nav {
   background-color: transparent;
   border-color: transparent;
 }
-
+.modalOpen {
+  width: 372px;
+  position: absolute;
+  margin-top: 355px;
+}
 img {
   position: relative;
+}
+
+.black-bg {
+  display: flex;
+  position: relative;
+  align-items: center;
+  width: 372px;
+  height: 631px;
+  background-color: rgba(0, 0, 0, 0.432);
+  top: -670px;
+}
+
+.white-bg {
+  width: 375px;
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.modal-exit-btn {
+  position: relative;
+  float: right;
+}
+
+.modal-exit-btn:hover {
+  cursor: pointer;
 }
 
 </style>
