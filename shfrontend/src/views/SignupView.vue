3b2c801db2a5c2f5975c043837babb25cff6ffb8
dmstn0526 <dmstn_0526@naver.com>
2023-02-23 17:39:55 +0900

    update용

--- shfrontend/src/views/SignupView.vue
+++ shfrontend/src/views/SignupView.vue
@@ -100,7 +100,7 @@ export default {
             }).catch(() => {
                 this.showModalfail = true;
                 this.failModal = true;
-            })
+            });
         }
     }
 }
